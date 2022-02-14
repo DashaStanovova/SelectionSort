8aae6bed31efb8ecd880d6bf0cfd9d121efdb37d
DashaStanovova <97908383+DashaStanovova@users.noreply.github.com>
2022-02-14 19:53:31 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-	
-    }
-}
+ARRAY = [2,6,34,5,1,7,8,26,9,11]
+loop FIRST from 0 to 9
+    LEAST = FIRST
+    loop CURRENT from FIRST+1 to 9
+        if ARRAY[CURRENT] < ARRAY[LEAST] then
+           LEAST = CURRENT
+        end if
+    end loop	  
+    TEMP = ARRAY[LEAST]
+    ARRAY[LEAST] = ARRAY[FIRST]
+    ARRAY[FIRST] = TEMP
+end loop
+loop I from 0 to 9
+    output ARRAY[I]
+end loop
